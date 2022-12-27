5fc553cc6b029191a029e40a84e8767eee828af6
MINICARNIGEL <grocerysynchronization@gmail.com>
2022-12-27 14:23:21 -0600

    js-basics progress

--- web-projects/solemn-pride/scripts/main.js
+++ web-projects/solemn-pride/scripts/main.js
@@ -2,11 +2,29 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "web-projects\solemn-pride\images\cross.png") {
-        myImage.setAttribute("src", "web-projects\solemn-pride\images\heart.png");
+    if (mySrc === "file:///C:/code/mdn/mdn-web-dev-course/web-projects/solemn-pride/images/cross.png") {
+        myImage.setAttribute("src", "images/heart.png");
     } else {
-        myImage.setAttribute("src", "web-projects\solemn-pride\images\cross.png");
+        myImage.setAttribute("src", "file:///C:/code/mdn/mdn-web-dev-course/web-projects/solemn-pride/images/cross.png");
     }
-};
+}; //toggles images of cross and sacred heart back and forth on click
 
-//test
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button"); 
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `God loves you, ${myName}`;
+} //set username prompt
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `God loves you, ${storedName}`;
+} //checks if username is stored, if so, displays stored name. if not, runs username prompt
+
+myButton.onclick = () => {
+    setUserName();
+}; //runs setUserName on button click
