3fc72934feb76514130291a95e20e3deec258a7d
grocerygoblin <grocerysynchronization@gmail.com>
2022-12-26 19:20:40 -0600

    testing js event

--- web-projects/solemn-pride/scripts/main.js
+++ web-projects/solemn-pride/scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.onClick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "D:\code\mdn\mdn-web-dev-course\web-projects\solemn-pride\images\cross.png") {
+        myImage.setAttribute("src", "D:\code\mdn\mdn-web-dev-course\web-projects\solemn-pride\images\heart.png");
+    } else {
+        myImage.setAttribute("src", "D:\code\mdn\mdn-web-dev-course\web-projects\solemn-pride\images\cross.png");
+    }
+};
