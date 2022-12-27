c0ea1bedd4222df8b0943d6954379811c99f8b50
MINICARNIGEL <grocerysynchronization@gmail.com>
2022-12-26 20:19:38 -0600

    swapped to relative filepaths

--- web-projects/solemn-pride/scripts/main.js
+++ web-projects/solemn-pride/scripts/main.js
@@ -2,9 +2,9 @@ const myImage = document.querySelector("img");
 
 myImage.onClick = () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "D:\code\mdn\mdn-web-dev-course\web-projects\solemn-pride\images\cross.png") {
-        myImage.setAttribute("src", "D:\code\mdn\mdn-web-dev-course\web-projects\solemn-pride\images\heart.png");
+    if (mySrc === "web-projects\solemn-pride\images\cross.png") {
+        myImage.setAttribute("src", "web-projects\solemn-pride\images\heart.png");
     } else {
-        myImage.setAttribute("src", "D:\code\mdn\mdn-web-dev-course\web-projects\solemn-pride\images\cross.png");
+        myImage.setAttribute("src", "web-projects\solemn-pride\images\cross.png");
     }
 };
